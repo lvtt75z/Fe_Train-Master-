b979b73634779e37525020a2be8f781a79d5adbc
HaiNam102 <bimhuongday@gmail.com>
2024-11-27 22:35:10 +0700

    ahiii

--- src/Component/Admin/Content/MealPlan/ManagerMealPlan.js
+++ src/Component/Admin/Content/MealPlan/ManagerMealPlan.js
@@ -37,7 +37,7 @@ const ManagerMealPlan = () => {
                     <TableMealPlan refresh={refreshTable} />
                 </div>
                 <CreateMealPlan
-                    show={showModalCreateMealPlan}
+                    // show={showModalCreateMealPlan}
                     setShow={setShowModelCreateMealPlan}
                     onAdd={handleDataAdded}
                 />
